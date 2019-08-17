cddada7b85aaeb3e14bf48dc11c5cd921ad1b114
David Foote <david.a.foote@gmail.com>
2019-08-17 23:12:46 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Chronicles of Narnia", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 21);
+INSERT INTO books (title, year, series_id) VALUES ("The Lion, the Witch, and the Wardrobe", 2009, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Magician's Nephew", 2009, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Prince Caspian", 2009, 3);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter: The Prequel", 2008, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Sorceror's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 3); 
